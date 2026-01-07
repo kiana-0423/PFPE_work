4617b46e94420889a677cb7cb16bafa264486b0b
kiana-0424 <huang1997c@gmail.com>
2026-01-07 19:21:51 +0900

    1.7

--- mole/sillicon/super.py
+++ mole/sillicon/super.py
@@ -3,6 +3,6 @@ from ase.build import surface
 
 atoms = read('Sio.cif')  
 slab = surface(atoms, (1, 0, 0), layers=1, vacuum=0.0, periodic=True)
-big = slab.repeat((41, 37, 6))
+big = slab.repeat((5, 5, 3))
 write('SiO2.cif',  big, format='cif')
 write("output.pdb", big)
